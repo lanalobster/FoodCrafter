17ca5d0893fa4d41e50d6de2118e90864f42ebd9
Orutsu <or4uk15@gmail.com>
2020-02-29 21:12:55 +0200

    new file

--- kitchen.js
+++ kitchen.js
@@ -3,21 +3,31 @@ const addIngredient = () => {
 	const list = document.getElementById("list-ingredient");
 	if (list.childNodes.length >= 10) {
 		return;
+	} else if (input.value === ""){
+		return;
 	}
 	const item = document.createElement('li');
+	item.id = `ingredientID${list.childNodes.length}`;
 	console.log(list.childNodes.length);
-	item.innerHTML = ` <div id="ingredientID${list.childNodes.length}" class="item-ingredient">
+	item.innerHTML = ` <div class="item-ingredient">
 				    	<div class="delete-button-block">
 				    		<button class="delete-button" onclick="removeIngredient(ingredientID${list.childNodes.length})">X</button>
 				    	</div>
 				    	<div class="ingredient-name-block">
 				    		<span class="ingredient-name">${input.value}</span>
 				    	</div>
-				    </div>`
-	list.appendChild(item);
+				    </div>`;
+	
+	if (list.childNodes.length <= 1) {
+		list.appendChild(item);
+		console.log("option 1");
+	} else {
+		list.insertBefore(item, list.childNodes[0]);
+		console.log("option 2");
+	}
+	input.value = "";
 }
 
 const removeIngredient = (ingredientID) => {
 	ingredientID.parentNode.removeChild(ingredientID);
-	console.log(ingredientID);
 }
