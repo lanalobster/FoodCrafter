caa0cbc03f1bdc5b9576d4e91b62af99b3399e8e
Roksolana Hudyma <lana.gudyma15@gmail.com>
2020-03-30 17:37:39 +0300

    Text color in print function changed.

--- index/print.js
+++ index/print.js
@@ -5,12 +5,14 @@ const toPrint = (x) => {
         document.getElementById("inspiration-block").style.display = 'none';
         document.getElementById("main-carousel").style.display = 'none';
         document.getElementById("switcher").style.display = 'none';
+        document.getElementById("desciption-block").style.color = 'black';
     } else {
         document.getElementById("main-header").style.display = 'flex';
         document.getElementById("popular-recipe-block").style.display = 'flex';
         document.getElementById("inspiration-block").style.display = 'flex';
         document.getElementById("main-carousel").style.display = 'block';
         document.getElementById("switcher").style.display = 'flex';
+        document.getElementById("desciption-block").style.color = '#1d737';
     }
 }
 
