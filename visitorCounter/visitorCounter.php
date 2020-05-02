927004585b7dd366c8a90b39265a495c1b5b7fb5
Orest Krasnevych <orest.krasnevych.pz.2018@lpnu.ua>
2020-05-02 14:03:25 +0300

    Small fixes

--- visitorCounter/visitorCounter.php
+++ visitorCounter/visitorCounter.php
@@ -1,8 +1,8 @@
 <?php
 //<Ляна>
-if(file_exists("visitorCount.txt"))
+if(file_exists("./visitorCounter/visitorCount.txt"))
 {
-  $visitorCountFile = fopen("visitorCount.txt", "r");
+  $visitorCountFile = fopen("./visitorCounter/visitorCount.txt", "r");
   $visitorCount = fgets($visitorCountFile, 255);
   if(!is_numeric($visitorCount))
     echo "The file contains not numeric data:" . $visitorCount;
@@ -10,14 +10,14 @@ if(file_exists("visitorCount.txt"))
   {
     ++$visitorCount;
     fclose($visitorCountFile);
-    $visitorCountFile = fopen("visitorCount.txt", "w");
+    $visitorCountFile = fopen("./visitorCounter/visitorCount.txt", "w");
     fputs($visitorCountFile, $visitorCount);
     fclose($visitorCountFile);
   }
 }
 else
 {
-  $visitorCountFile = fopen("visitorCount.txt", "w");
+  $visitorCountFile = fopen("./visitorCounter/visitorCount.txt", "w");
   fputs($visitorCountFile, "1");
   fclose($visitorCountFile);
 }
